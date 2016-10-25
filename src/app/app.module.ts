57d4fb0f163ca8a8855c489f28bfe59137d87492
Aitor Ibañez <freelance@aitoribanez.com>
2016-10-26 01:06:53 +0200

    routes added + about example component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+
+const routing = RouterModule.forRoot([
+    { path: '',      component: AppComponent },
+    { path: 'about', component: AboutComponent }
+])
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent
   ],
   imports: [
     FormsModule,
     BrowserModule,
-    HttpModule
+    HttpModule,
+    RouterModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
