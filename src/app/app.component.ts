15e76f76ffba22211f5e279f44dde1aa42a26167
Aitor Ibañez <freelance@aitoribanez.com>
2016-10-26 14:34:26 +0200

    service binded with HomeComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,9 @@
 import { Component } from '@angular/core';
 
-import ApiWrapperService from './lib/apiWrapper.service';
-import { Product } from './entitys/product'
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ApiWrapperService],
+  styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  title: string = 'app works!';
-  products: Array<Product>;
-
-  constructor(api: ApiWrapperService) {
-    api
-      .get('products')
-      .subscribe(products => { 
-        console.log(products);
-        this.products = products
-      });  
-  }
-  
-}
+export class AppComponent {}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import ApiWrapperService from '../../lib/apiWrapper.service';
+import { Product } from '../../entitys/product'
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ApiWrapperService],
 })
-export class HomeComponent implements OnInit {
 
-  constructor() { }
+export class HomeComponent implements OnInit {
+  title: string = 'app works!';
+  products: Array<Product>;
+
+  constructor(api: ApiWrapperService) {
+    api
+      .get('products')
+      .subscribe(products => { 
+        console.log(products);
+        this.products = products
+      });  
+  }
 
   ngOnInit() {
   }
