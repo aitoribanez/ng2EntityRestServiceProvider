feb23addb47312c3d2c645170b59c313c91aab94
Aitor Ibañez <freelance@aitoribanez.com>
2016-10-28 21:30:55 +0200

    Add validation to one way template driven and code driven form

--- src/app/components/form/code-driven-form/code-driven-form.component.ts
+++ src/app/components/form/code-driven-form/code-driven-form.component.ts
@@ -14,16 +14,27 @@ class User {
 export class CodeDrivenFormComponent implements OnInit {
   usernameCtrl: FormControl;
   passwordCtrl: FormControl;
+  birthdateCtrl: FormControl;
   userForm: FormGroup;
+
+  static isOldEnough(control: FormControl) {
+    // control is a date input, so we can build the Date from the value
+    const birthDatePlus18 = new Date(control.value);
+    birthDatePlus18.setFullYear(birthDatePlus18.getFullYear() + 18); 
+    return birthDatePlus18 < new Date() ? null : { tooYoung: true };
+  }
 
   constructor(fb: FormBuilder) {
     this.usernameCtrl = fb.control('', Validators.compose([Validators.required, Validators
 .minLength(3)]));
     this.passwordCtrl = fb.control('', Validators.required);
- 
+    this.birthdateCtrl = fb.control('', Validators.compose([Validators.required,
+CodeDrivenFormComponent.isOldEnough]));
+
     this.userForm = fb.group({
       username: this.usernameCtrl,
-      password: this.passwordCtrl
+      password: this.passwordCtrl,
+      birthdate: this.birthdateCtrl
     });
   }
 
